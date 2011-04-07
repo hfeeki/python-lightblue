637c850faafd674de959112be9acfcbc4e56553a
Ryan <ryan@archweblabs.com>
2011-04-07 13:38:53 -0700

    Updated files with LightAquaBlue framework for Mac OS X 10.6

--- src/series60/_lightblueutil/_lightblueutil.cpp
+++ src/series60/_lightblueutil/_lightblueutil.cpp
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2009 Bea Lam. All rights reserved.
- *
- * This file is part of LightBlue.
- *
- * LightBlue is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * LightBlue is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with LightBlue.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 // This file provides a Python extension for performing operations not offered 
 // in the standard Python for Series 60 API, including:
 //  - non-GUI discovery
